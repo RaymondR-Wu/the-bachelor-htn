4aa42ac1d93c25c28d0c7cf9afd8a52f511ce633
Catherine Burns <catherineburns8@gmail.com>
2021-01-17 04:12:42 -0500

    Pit -> Hive

--- src/Pits.js
+++ src/Pits.js
@@ -115,20 +115,20 @@ export default class Pits extends React.Component{
                     <Menu vertical style = {{border: 'none', borderColor: 'yellow', width: '100%', flex: 'column', margin: '10px auto'}}>
                         <Menu.Item onClick = {() => {this.clickedChat(0)}} style = {{height: '10vh', borderColor: 'black', border: 'none'}} active = {this.state.activeChatIdx === 0}>
                             <div style = {{textAlign: 'left'}}>
-                                Pit 1 &#128521;
+                                Hive 1 &#128521;
                             </div>
                             <div style = {{display: 'flex', flexDirection: 'row', justifyContent: 'space-between'}}>
                                 <div style = {{left: '0px'}}> W/ Channing Tatum, Zac Efron, ... </div>
                                 <div style = {{right: '0px'}}> Mr.Tatum: Hey </div>
                             </div>
-                        
+
                         </Menu.Item>
 
                         <Menu.Item onClick = {() => {this.clickedChat(1)}} style = {{height: '10vh', borderColor: 'black', border: 'none'}} active = {this.state.activeChatIdx === 1} >
                             <div style = {{textAlign: 'left'}}>
-                                Pit 2
+                                Hive 2
                             </div>
-                            
+
                             <div style = {{display: 'flex', flexDirection: 'row', justifyContent: 'space-between'}}>
                                 <div style = {{left: '0px'}}> W/ Sven, Shrek, Olaf... </div>
                                 <div style = {{right: '0px'}}> Olaf: Wait till summer </div>
@@ -137,34 +137,34 @@ export default class Pits extends React.Component{
 
                         <Menu.Item onClick = {() => {this.clickedChat(2)}} style = {{height: '10vh', borderColor: 'black', border: 'none'}} active = {this.state.activeChatIdx === 2}>
                             <div style = {{textAlign: 'left'}}>
-                                Pit 3
+                                Hive 3
                             </div>
                             <div style = {{display: 'flex', flexDirection: 'row', justifyContent: 'space-between'}}>
                                 <div style = {{left: '0px'}}> W/ Dwight Schrute, Janice, ... </div>
                                 <div style = {{right: '0px'}}> Janice: O MY  </div>
                             </div>
-                            
+
                         </Menu.Item>
                         <Menu.Item style = {{height: '10vh', borderColor: 'black', border: 'none'}} >
                             <div style = {{textAlign: 'left'}}>
                                 +
                             </div>
-                            Browse Open Pits
+                            Browse Open Hives
                         </Menu.Item>
                         <Menu.Item style = {{height: '10vh', borderColor: 'black', border: 'none'}} >
                             <div style = {{textAlign: 'left'}}>
                                 +
                             </div>
-                            Create Your Own Pit
+                            Create Your Own Hive
                         </Menu.Item>
                     </Menu>
 
                 </div>
 
                 {/* <AnswerPage  /> */}
-                <div style = {{position: 'relative', width: '100%', border: 'none', borderColor: 'orange', marginTop: '10px'}}> 
+                <div style = {{position: 'relative', width: '100%', border: 'none', borderColor: 'orange', marginTop: '10px'}}>
                     <Button onClick = {() => this.clickedBack()} labelPosition = "left" icon="left chevron" content = "Back" style = {{left: '0px', position: "absolute", margin: "0px 0px 0px 30px"}}/>
-                    {this.imageList()} 
+                    {this.imageList()}
                     <div style = {{position: 'relative', top: '23vh', left: '50%', height: '10%', width: '10%', alignItems: 'center', textAlign: 'center', color: 'white'}}>
                         WHO SHALL BE ELIMINATED?
                         <Button onClick = {() => this.onSubmit()} style = {{marginTop: "2vh", height: '10vh', width: '10vh', borderRadius: '5vh', textAlign: 'center', justifyContent: 'center', alignItems: 'center', fontSize: '90%', padding: '0px'}}>
